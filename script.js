aa84eecd63292edfedfa91af669eb56615b71691
Shakti Sharma <shaktisharma8982@gmail.com>
2025-07-23 20:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function timer() {
-    for(let i = 1; i<=5; i++){
-        setTimeout(function() {
-            console.log(i);
-        }, i*1000);
-    }
-    console.log("Hello ninjas!");
-}
-timer();
+
